023181361b0d0df2248a87d0e4a10a1f045dca14
zhu121705 <zhu121705@gmail.com>
2025-02-02 21:45:51 -0500

    fault exe time folder

---
+++ query/reliability/fault_exe_time.py
@@ -0,0 +1,12 @@
+time_between_errors_expectation = 1.8591717547119724
+checkpoint_intervals = 1
+rollback_time = 0.1
+time_between_errors = time_between_errors_expectation
+execution_time = 10
+checkpoint_setup_time = 0.1
+
+def calculate_total_time(time_between_errors, checkpoint_intervals, checkpoint_time, execution_time, rollback_time):
+
+	return(total_time)
+
+calculate_total_time(time_between_errors, checkpoint_intervals, checkpoint_setup_time, execution_time, rollback_time)
